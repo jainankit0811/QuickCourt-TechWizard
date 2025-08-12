10b8931bdbf990d8bc61793f6757484770eee194
HARSHVARDHAN BARETH <137777618+HARSHVARDHAN-BARETH@users.noreply.github.com>
2025-08-12 09:57:24 +0530

    update

--- frontend/src/screen/LandingPage.jsx
+++ frontend/src/screen/LandingPage.jsx
@@ -24,7 +24,7 @@ const LandingPage = () => {
             className="text-white bg-gradient-to-r from-blue-500 via-blue-600 to-blue-700 hover:bg-gradient-to-br 
                        focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 w-full"
                 >
-                    <Link to="/Signup"><h2>Go Ahead</h2></Link>
+                    <Link to="/Signup"><h2 className="text-white text-xl">Go Ahead</h2></Link>
                 </button>
             </div>
         </div>
